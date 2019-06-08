3b532a00ec0e19e9c01f8be19924c9cce41a0545
Travis <travisboss@gmail.com>
2019-06-08 12:27:43 -0500

    update to picnic event

--- js/vue.js
+++ js/vue.js
@@ -29,7 +29,7 @@ Vue.component('vue-announcements', {
             <div class="container">
                 <div class="columns is-centered">
                     <div class="column is-half">
-                    <div class="fb-post" data-href="https://www.facebook.com/chilenos.fernandez.9/posts/476209206486595:0" data-width="750" data-show-text="true"><blockquote cite="https://developers.facebook.com/chilenos.fernandez.9/posts/476209206486595:0" class="fb-xfbml-parse-ignore"><p>Actividades del club chilenos en Minnesota 2019!</p>Posted by <a href="https://www.facebook.com/chilenos.fernandez.9">Chilenos EnMinnesota</a> on&nbsp;<a href="https://developers.facebook.com/chilenos.fernandez.9/posts/476209206486595:0">Saturday, May 18, 2019</a></blockquote></div>
+                    <div class="fb-post" data-href="https://www.facebook.com/chilenos.fernandez.9/posts/490712681702914" data-width="750" data-show-text="true"><blockquote cite="https://developers.facebook.com/chilenos.fernandez.9/posts/490712681702914" class="fb-xfbml-parse-ignore">Posted by <a href="https://www.facebook.com/chilenos.fernandez.9">Chilenos EnMinnesota</a> on&nbsp;<a href="https://developers.facebook.com/chilenos.fernandez.9/posts/490712681702914">Saturday, June 8, 2019</a></blockquote></div>
                    </div>
                 </div>
             </div>
